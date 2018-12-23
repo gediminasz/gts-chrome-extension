8bc845c5c21188a0ccf2960227819090f3c92ee0
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-12-23 21:20:40 +0200

    Use mithril for rendering

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
+const CONTAINER_ID = "js-pex-container";
+const CHART_COLOR = "#1b2c3d";
+
 function inject() {
     const userId = getUserId();
     const appMountRoot = document.getElementById("app_mount_root");
 
-    appMountRoot.insertAdjacentHTML("afterend", '<div style="width: 800px; margin: auto"><canvas id="gts-pex-sr" width="800" height="400" /></div>');
-    appMountRoot.insertAdjacentHTML("afterend", '<div style="width: 800px; margin: auto"><canvas id="gts-pex-dr" width="800" height="400" /></div>');
+    appMountRoot.insertAdjacentHTML("afterend", `<div id="${CONTAINER_ID}">Loading...</div>`);
 
-    fetchStatsHistory(userId).then(history => {
-        const driverRatingHistory = collectStats(history, "stats12");
-        const sporstmanshipRatingHistory = collectStats(history, "stats13");
-
-        renderChart("gts-pex-dr", driverRatingHistory, "#1b2c3d");
-        renderChart("gts-pex-sr", sporstmanshipRatingHistory, "#1b2c3d");
-    });
+    fetchStatsHistory(userId).then(
+        history => m.mount(document.getElementById(CONTAINER_ID), Container(history))
+    );
 }
 
 function getUserId(pattern = /us\/gtsport\/user\/profile\/(\d+)\/overview/) {
@@ -37,9 +35,36 @@ function collectStats(stats, key) {
         .map(value => parseInt(value));
 }
 
-function renderChart(elementId, series, color) {
-    return new Chart(elementId, {
-        type: 'line',
+function Container(history) {
+    const driverRatingHistory = collectStats(history, "stats12");
+    const sporstmanshipRatingHistory = collectStats(history, "stats13");
+
+    return {
+        view: () => [
+            m(RatingChart("Driver Rating", driverRatingHistory)),
+            m(RatingChart("Sportsmanship Rating", sporstmanshipRatingHistory))
+        ]
+    }
+}
+
+function RatingChart(title, series) {
+    return {
+        view: () => (
+            m("div", { style: { textAlign: "center" } }, [
+                m("h1", title),
+                m(
+                    "div",
+                    { style: { width: "800px", height: "400px", margin: "auto" } },
+                    m("canvas", { width: 800, height: 400, oncreate: (vnode) => renderChart(vnode.dom, series) })
+                )
+            ])
+        )
+    }
+}
+
+function renderChart(element, series) {
+    return new Chart(element, {
+        type: "line",
         data: {
             datasets: [
                 {
@@ -47,14 +72,14 @@ function renderChart(elementId, series, color) {
                     steppedLine: "after",
                     fill: false,
                     pointRadius: 0,
-                    borderColor: color,
-                    backgroundColor: color
+                    borderColor: CHART_COLOR,
+                    backgroundColor: CHART_COLOR
                 }
             ]
         },
         options: {
             scales: {
-                xAxes: [{ type: 'linear' }],
+                xAxes: [{ type: "linear" }],
                 yAxes: [{
                     ticks: {
                         beginAtZero: true

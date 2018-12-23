a9c0af75a2f039034862a0961499fb53066f0e88
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-12-23 20:41:59 +0200

    Refactor renderStatsHistory

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ function inject() {
     appMountRoot.insertAdjacentHTML("afterend", '<div style="width: 800px; margin: auto"><canvas id="gts-pex-sr" width="800" height="400" /></div>');
     appMountRoot.insertAdjacentHTML("afterend", '<div style="width: 800px; margin: auto"><canvas id="gts-pex-dr" width="800" height="400" /></div>');
 
-    renderStatsHistory(userId);
+    fetchStatsHistory(userId).then(history => {
+        const driverRatingHistory = collectStats(history, "stats12");
+        const sporstmanshipRatingHistory = collectStats(history, "stats13");
+
+        renderChart("gts-pex-dr", driverRatingHistory, "#1b2c3d");
+        renderChart("gts-pex-sr", sporstmanshipRatingHistory, "#1b2c3d");
+    });
 }
 
 function getUserId(pattern = /us\/gtsport\/user\/profile\/(\d+)\/overview/) {
     return pattern.exec(location.href)[1];
 }
 
-function renderStatsHistory(userId) {
+function fetchStatsHistory(userId) {
     const body = new FormData;
     body.append("job", 12);
     body.append("month_begin", 10);
@@ -20,15 +26,9 @@ function renderStatsHistory(userId) {
     body.append("user_no", userId);
     body.append("year_begin", 2017);
     body.append("year_end", 2050);
-    fetch("https://www.gran-turismo.com/us/api/gt7sp/profile/", { method: "POST", body })
+    return fetch("https://www.gran-turismo.com/us/api/gt7sp/profile/", { method: "POST", body })
         .then(response => response.json())
-        .then(({ stats_history: history }) => {
-            const driverRatingHistory = collectStats(history, "stats12");
-            const sporstmanshipRatingHistory = collectStats(history, "stats13");
-
-            renderChart("gts-pex-dr", driverRatingHistory, "#1b2c3d");
-            renderChart("gts-pex-sr", sporstmanshipRatingHistory, "#1b2c3d");
-        });
+        .then(({ stats_history }) => stats_history);
 }
 
 function collectStats(stats, key) {
